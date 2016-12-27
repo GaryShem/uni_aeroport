a9e4303ead874b4f808a5c961b1e539744add84c
Gary Shem <tassadarrr@gmail.com>
2016-12-27 19:11:07 +0300

    bus handler added

---
+++ Bus/BusHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace Bus
+{
+    public static class BusHandler
+    {
+        private static Thread BusHandlerThread;
+        static BusHandler()
+        {
+            BusHandlerThread = new Thread(HandleBus);
+        }
+
+        private static void HandleBus()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                //добавлять действия сюда
+            }
+        }
+    }
+}
